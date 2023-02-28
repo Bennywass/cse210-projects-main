e54faf952dc44606cfa6a3ba37058728bb0afedb
bennywass <bennywass4@gmail.com>
2023-02-28 15:16:34 -0700

    New Repository

---
+++ csharp-prep/Prep2/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What did you get on the recent test? ");
+        string numberGrade = Console.ReadLine();
+        int number = int.Parse(numberGrade);
+    }
+}
